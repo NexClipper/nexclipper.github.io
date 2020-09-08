f36f167a99c7ace5eaa3c0a1662a52842ec291c8
ddiiwoong <ddiiwoong@gmail.com>
2020-09-08 14:02:58 +0900

    change home image txt

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,32 +8,29 @@ import styles from './styles.module.css';
 
 const features = [
   {
-    title: 'Easy to Use',
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    title: 'Cloud Native',
+    imageUrl: 'img/cloudnative-2-200x200.png',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        A consolidated platform for Cloud Native Observability & Delivery
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    title: 'Prometheus Ecosystem',
+    imageUrl: 'img/Prometheus_3d.png',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Prometheus-based observability platform for any cloud native infrastructure
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    imageUrl: 'img/undraw_docusaurus_react.svg',
+    title: 'Enterprise Architecture',
+    imageUrl: 'img/enterprisearchitecture-200x200.png',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Manage Multi-Cluster & Multi-Tenancy Monitoring with Global Views and Limitless Metrics Retention
       </>
     ),
   },
