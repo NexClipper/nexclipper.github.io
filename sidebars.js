e8fa22abb5384ebba86d780c1bb389748a6d9513
ddiiwoong <ddiiwoong@gmail.com>
2021-02-02 15:50:01 +0900

    change sidebars

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    'Getting Started': ['intro', 'local', 'start'],
+    'Getting Started': ['intro', 'local', 'start_en'],
     'Docs Use Guide': ['style', 'mdx','info'],
   },
 };
