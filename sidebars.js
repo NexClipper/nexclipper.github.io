be61f6641ba17a6a8877cd39f851a8f961218234
ddiiwoong <ddiiwoong@gmail.com>
2021-02-03 14:32:54 +0900

    change sidemenu

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,46 @@
 module.exports = {
-  someSidebar: {
-    'Getting Started': ['intro', 'install'],
-    'Docs Use Guide': ['style', 'mdx','info'],
-  },
+  someSidebar: [
+    {
+      'Getting Started': ['intro', 'install'],
+    },
+    {
+      type: 'category',
+      label: 'Products',
+      items: [
+        {
+          type: 'link',
+          label: 'Klevr', 
+          href: 'https://github.com/NexClipper/klevr' 
+        },
+        {
+          type: 'link',
+          label: 'Provbee', 
+          href: 'https://github.com/NexClipper/provbee' 
+        },
+        {
+          type: 'link',
+          label: 'NexClipperBasic',
+          href: 'https://github.com/NexClipper/NexClipperBasic'
+        },
+        {
+          type: 'link',
+          label: 'webstork',
+          href: 'https://github.com/NexClipper/webstork'
+        },
+        {
+          type: 'link',
+          label: 'exporterhub.io',
+          href: 'https://github.com/NexClipper/exporterhub.io'
+        },
+        {
+          type: 'link',
+          label: 'metric-operator',
+          href: 'https://github.com/NexClipper/metric-operator'
+        },
+      ],
+    },
+    {
+      'Docs Use Guide': ['info', 'style'],
+    },
+  ],
 };
